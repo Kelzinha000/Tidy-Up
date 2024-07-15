4293a6b447146d0cc2aa8ba8d23b0659ea7bdcfb
unknown <raquelbulhoes212@gmail.com>
2024-07-15 16:41:16 -0300

    Header feito

---
+++ src/Header/MenuMobile.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {FaTimes} from "react-icons/fa";
+import {ContainerNav, Content, Logo} from "../styled/App.js"
+import imgLogoBranca from "../image/Logo-TidyUp-branca.png"
+
+const MenuMobile = ({active}) => {
+    const fecharMenu = () =>{
+        active(false)
+    }
+
+    return(
+        <>
+        <ContainerNav menumobile ={active}>
+            <FaTimes className="svg-fechar" onClick={fecharMenu}/>
+            <Content>
+            <h1>Home</h1>
+            <h1>Sobre</h1>
+            <Logo src={imgLogoBranca} alt="" />
+
+            
+            </Content>
+        </ContainerNav>
+        </>
+    )
+}
+
+export default MenuMobile;
